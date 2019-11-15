5cc0906d9b8d11065ba4e2198ffc558165743c44
mariellaPariente <42952666+mariellaPariente@users.noreply.github.com>
2019-11-15 00:56:55 -0500

    Add files via upload

---
+++ markov_text_generator1.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.insert(0, "/usr/local/lib/python3.6/dist-packages/")
+import markovify
+
+with open("trump_tweets.txt") as f:
+    text = f.read()
+model_a = markovify.Text(text, state_size=2)
+
+with open("lovecraft.txt") as f:
+    text = f.read()
+model_b = markovify.Text(text, state_size=2)
+
+model_combo = markovify.combine([model_a, model_b], [1.0, 20.0])
+
+# Print five randomly-generated sentences
+for i in range(100):
+    print(model_combo.make_sentence())
+    print ''
